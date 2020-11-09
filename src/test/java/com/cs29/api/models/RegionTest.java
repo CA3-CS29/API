fb4117dd0318015e083cecc3120e1a8ae96f5277
Jack Tickoo <2397224t@student.gla.ac.uk>
2020-11-09 20:33:01 +0000

    Changed models UserIDs from strings to Lists and altered tests accordingly.

--- src/test/java/com/cs29/api/models/RegionTest.java
+++ src/test/java/com/cs29/api/models/RegionTest.java
@@ -4,24 +4,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegionTest {
     private final ObjectMapper MAPPER = new ObjectMapper();
-    private final String TEST_ID = "TEST_ID";
+    private final List<String> TEST_ID = Arrays.asList("TEST_ID1", "TEST_ID2");
     private final String TEST_NAME = "TEST_NAME";
     private final int TEST_NUM_OFFICES = 0;
     private final Region TEST_REGION = Region
             .builder()
-            .region_id(TEST_ID)
-            .portfolio_id(TEST_ID)
+            .region_id(TEST_NAME)
+            .portfolio_id(TEST_NAME)
             .user_id(TEST_ID)
             .num_offices(TEST_NUM_OFFICES)
             .name(TEST_NAME)
             .offices(null)
             .build();
-    private final String TEST_SERIALIZED_REGION = "{\"region_id\":\"TEST_ID\",\"portfolio_id\":\"TEST_ID\"," +
-            "\"user_id\":\"TEST_ID\",\"name\":\"TEST_NAME\",\"num_offices\":0,\"offices\":null}";
+    private final String TEST_SERIALIZED_REGION = "{\"region_id\":\"TEST_NAME\",\"portfolio_id\":\"TEST_NAME\"," +
+            "\"user_id\":[\"TEST_ID1\",\"TEST_ID2\"],\"name\":\"TEST_NAME\",\"num_offices\":0,\"offices\":null}";
 
     @SneakyThrows
     @Test
